3b0e2a391215b2647cda318c6bd445694dcccc39
张定邦 <zdbzuishuai@sjtu.edu.cn>
2019-10-26 11:42:25 +0800

    zdb2

--- readInTrade.py
+++ readInTrade.py
@@ -1,46 +1,53 @@
 import time
 from bot import read_from_exchange
 
-bond = []
-car = []
-che = []
-bdu = []
-ali = []
-tct = []
-bat = []
 
-def read_in_trade(exchange):
-    count = 0
-    while (count < 200):
-        info = read_from_exchange(exchange)
-        if not info:
-            break
-        type = info["type"]
-        if (type == "close"):
-            print("Server closed.")
-            return
-        if (type == "trade"):
 
-            if (info["symbol"] == "BOND"):
-                bond.append(info["price"])
+class Data():
+    def __init__(self):
+        self.bond = []
+        self.car = []
+        self.che = []
+        self.bdu = []
+        self.ali = []
+        self.tct = []
+        self.bat = []
+    
+    def get_data(self):
+        return self.bond,self.car,self.che,self.bdu,self.ali,self.tct,self.bat
 
-            if (info["symbol"] == "CAR"):
-                car.append(info["price"])
+    def read_in_trade(self,exchange):
+        count = 0
+        while (count < 200):
+            info = read_from_exchange(exchange)
+            if not info:
+                break
+            type = info["type"]
+            if (type == "close"):
+                print("Server closed.")
+                return
+            if (type == "trade"):
 
-            if (info["symbol"] == "CHE"):
-                che.append(info["price"])
+                if (info["symbol"] == "BOND"):
+                    self.bond.append(info["price"])
 
-            if (info["symbol"] == "BDU"):
-                bdu.append(info["price"])
+                if (info["symbol"] == "CAR"):
+                    self.car.append(info["price"])
 
-            if (info["symbol"] == "ALI"):
-                ali.append(info["price"])
+                if (info["symbol"] == "CHE"):
+                    self.che.append(info["price"])
 
-            if (info["symbol"] == "TCT"):
-                tct.append(info["price"])
+                if (info["symbol"] == "BDU"):
+                    self.bdu.append(info["price"])
 
-            if (info["symbol"] == "BAT"):
-                bat.append(info["price"])
+                if (info["symbol"] == "ALI"):
+                    self.ali.append(info["price"])
 
-        time.sleep(0.01)
-        count += 1
+                if (info["symbol"] == "TCT"):
+                    self.tct.append(info["price"])
+
+                if (info["symbol"] == "BAT"):
+                    self.bat.append(info["price"])
+
+            time.sleep(0.01)
+            count += 1
