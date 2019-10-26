7b740e561b1eb5f44081de370eb190b02721121f
nieliangxu <yk8288196@126.com>
2019-10-26 16:37:38 +0800

    BAT

--- BAT_strategy.py
+++ BAT_strategy.py
@@ -60,9 +60,12 @@ def operate_car(exchange, message, data):
             positive_order.append(sell_symbol(exchange, "ALI", ALI_sell_price-1, 3*size))
             positive_order.append(sell_symbol(exchange, "TCT", TCT_sell_price-1, 2*size))
 
-        elif (10 * BAT_mean_price > (3 * BOND_mean_price + 2 * BDU_mean_price + 3 * ALI_mean_price + 2 * TCT_mean_price)):
             for order_id in positive_order:
                 cancel_id(exchange, order_id)
+
+        # elif (10 * BAT_mean_price > (3 * BOND_mean_price + 2 * BDU_mean_price + 3 * ALI_mean_price + 2 * TCT_mean_price)):
+        #     for order_id in positive_order:
+        #         cancel_id(exchange, order_id)
 
 
         if (10 * BAT_mean_price - 100 > (3 * BOND_mean_price + 2 * BDU_mean_price + 3 * ALI_mean_price + 2 * TCT_mean_price)):
@@ -76,6 +79,9 @@ def operate_car(exchange, message, data):
 
             negative_order.append(sell_symbol(exchange, "BAT", BAT_sell_price - 1, 10*size))
 
-        elif (10 * BAT_mean_price < (3 * BOND_mean_price + 2 * BDU_mean_price + 3 * ALI_mean_price + 2 * TCT_mean_price)):
             for order_id in negative_order:
                 cancel_id(exchange, order_id)
+
+        # elif (10 * BAT_mean_price < (3 * BOND_mean_price + 2 * BDU_mean_price + 3 * ALI_mean_price + 2 * TCT_mean_price)):
+        #     for order_id in negative_order:
+        #         cancel_id(exchange, order_id)
