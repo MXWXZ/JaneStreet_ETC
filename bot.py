831d41714f6a000572325dd85e0b9b47337b5b7b
MXWXZ <me@imwxz.com>
2019-10-26 10:43:13 +0800

    bond

--- bot.py
+++ bot.py
@@ -58,14 +58,20 @@ def main():
     # Since many write messages generate marketdata, this will cause an
     # exponential explosion in pending messages. Please, don't do that!
     print("The exchange replied:", hello_from_exchange, file=sys.stderr)
+
     write_to_exchange(exchange, {"type": "add", "order_id": 1,
-                                 "symbol": "BOND", "dir": "BUY", "price": 999, "size": 1000})
+                                 "symbol": "BOND", "dir": "BUY", "price": 999, "size": 10000})
+    message = read_from_exchange(exchange)
+    print("The exchange replied:", message, file=sys.stderr)
     write_to_exchange(exchange, {"type": "add", "order_id": 2,
-                                 "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 1000})
+                                 "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 10000})
+    message = read_from_exchange(exchange)
+    print("The exchange replied:", message, file=sys.stderr)
 
     while True:
         message = read_from_exchange(exchange)
-        print("The exchange replied:", message, file=sys.stderr)
+        if 'fill' in message:
+            print("The exchange replied:", message, file=sys.stderr)
 
 
 if __name__ == "__main__":
