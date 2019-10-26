7b5b48286f85d11f1d518af8076be402b1b01d3e
张定邦 <zdbzuishuai@sjtu.edu.cn>
2019-10-26 13:50:31 +0800

    zdb9

--- strategy.py
+++ strategy.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from readInTrade import Data
-from bot import sell_symbol,buy_symbol,write_to_exchange
+from bot import sell_symbol,buy_symbol,write_to_exchange,convert
 
 
 def mean_value():
@@ -29,12 +29,7 @@ def is_exchange_BAT(BAT_price, BOND_price, BDU_price, ALI_price, TCT_price):
     """
     return how many we buy least BAT can get get_value profit
     """
-<<<<<<< HEAD
-    price_isGood = -(BAT_price*10 - BOND_price*3 - BDU_price * 2 - ALI_price * 3 -TCT_price * 2)
-=======
-    price_isGood = BAT_price * 10 - BOND_price * 3 - \
-        BDU_price * 2 - ALI_price * 3 - TCT_price * 2
->>>>>>> bfbe12128745b2cbb1c57d23159a036665790ac5
+    price_isGood = BAT_price * 10 - BOND_price * 3 - BDU_price * 2 - ALI_price * 3 - TCT_price * 2
     n = 100 / price_isGood
     n = int(n + 1) * 10
     get_value = n * price_isGood - 100
@@ -51,11 +46,11 @@ def is_exchange_CHE_or_CAR(CHE_price, CAR_price):
     if (CHE_price > CAR_price):
         num = 10 / (CHE_price - CAR_price)
         num = int(num + 1)
-        flag = "Buy_CHE"
+        flag = "Buy_CAR"
     else:
         num = 10 / (CAR_price - CHE_price)
         num = int(num + 1)
-        flag = "Buy_CAR"
+        flag = "Buy_CHE"
 
     return flag, num
 
@@ -64,11 +59,6 @@ def is_exchange_CHE_or_CAR(CHE_price, CAR_price):
 下面这一段加到bot里面
 """
 
-def convert(exchange, symbol, price, size):
-    global idcnt
-    write_to_exchange(exchange, {"type": "convert", "order_id": idcnt,
-                                 "symbol": symbol, "dir": "SELL", "price": price, "size": size})
-    idcnt += 1
 
 
 def is_exchange_four_stocks(BAT_price, BOND_price, BDU_price, ALI_price, TCT_price
@@ -76,10 +66,12 @@ def is_exchange_four_stocks(BAT_price, BOND_price, BDU_price, ALI_price, TCT_pri
     # NUM是现有的持仓, 买入股票，卖出BAT
     price_isGood = BAT_price * 10 - BOND_price * 3 - BDU_price * 2 - ALI_price * 3 - TCT_price * 2   # 可以套利的价格
     if price_isGood < 0:  # bat的价格比较低，买入bat，由邦总的程序处理
-        pass
-    else:  # bat 的价格比较高，买入四只股票的某一种或者某几种，
-        
+        tmp = min([(100 - BOND_NUM) / 3, (100 - BDU_NUM) / 2, (100 - ALI_NUM) / 3, (100 - TCT_NUM) / 2])
+        # 由于仓位的限制，tmp是最多的股票买入单位，其他的买多了也没法合成
+        tmp = min(100 - BAT_NUM, 10 * tmp)
 
+
+    else:  # bat 的价格比较高，买入四只股票的某一种或者某几种，
         tmp = min([(100 - BOND_NUM) / 3, (100 - BDU_NUM) / 2, (100 - ALI_NUM) / 3, (100 - TCT_NUM) / 2])
         # 由于仓位的限制，tmp是最多的股票买入单位，其他的买多了也没法合成
         tmp = min(100 - BAT_NUM, 10 * tmp)        
@@ -100,7 +92,14 @@ def is_exchange_four_stocks(BAT_price, BOND_price, BDU_price, ALI_price, TCT_pri
 
 
 
-data_now = Data()
-data_now.read_in_trade(exchange)
 bond, car, che, bdu, ali, tct, bat = data_now.get_data()
-now_marketbuy, now_marketsell = data_now.read_now_market
+batprice = sum(bat[-30:])/30
+bong_price = sum(bond[-30:])/30
+bdu_price = sum(bdu[-30:])/30
+ali_price = sum(ali[-30:])/30
+tct_price = sum(tct[-30:])/30
+car_price = sum(tct[-30:])/30
+che_price = sum(tct[-30:])/30
+
+num,flag = is_exchange_four_stocks()
+
