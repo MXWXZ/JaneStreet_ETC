71c67d4fce13d9dd7e4054e610eefedc2afe2ef4
张定邦 <zdbzuishuai@sjtu.edu.cn>
2019-10-26 14:16:39 +0800

    zdb11

--- strategy.py
+++ strategy.py
@@ -101,32 +101,35 @@ def is_exchange_four_stocks(BAT_price, BOND_price, BDU_price, ALI_price, TCT_pri
         sell_symbol(exchange, 'BAT', BAT_price, BAT_NUM)
 
 
-bond, car, che, bdu, ali, tct, bat = data_now.get_data()
-batprice = sum(bat[-30:]) / 30
-bong_price = sum(bond[-30:]) / 30
-bdu_price = sum(bdu[-30:]) / 30
-ali_price = sum(ali[-30:]) / 30
-tct_price = sum(tct[-30:]) / 30
-car_price = sum(tct[-30:]) / 30
-che_price = sum(tct[-30:]) / 30
 
-book = data_now.read_now_market()
-buyBAT, sellBAT = book['BAT']
-buyCAR, sellCAR = book['CAR']
-buyCHE, sellCHE = book['CHE']
-buyBDU, sellBDU = book['DBU']
-buyALI, sellALI = book['ALI']
-buyTCT, sellTCT = book['TCT']
-buyBOND, sellBOND = book['BOND']
-"""
-[[4316, 1], [4314, 1], [4311, 1], [4297, 1], [4294, 3], [4291, 2], [4286, 4]] 买一
-[[4337, 1], [4338, 1], [4345, 1], [4355, 1]] 卖一
-"""
+def buy_sell_CHE_or_CAR(exchange, message, data_now):
+    bond, car, che, bdu, ali, tct, bat = data_now.get_data()
+    batprice = sum(bat[-30:]) / 30
+    bong_price = sum(bond[-30:]) / 30
+    bdu_price = sum(bdu[-30:]) / 30
+    ali_price = sum(ali[-30:]) / 30
+    tct_price = sum(tct[-30:]) / 30
+    car_price = sum(tct[-30:]) / 30
+    che_price = sum(tct[-30:]) / 30
 
-if (len(car_price) > 0 and len(che_price) > 0):
-    flag, num = is_exchange_CHE_or_CAR(che_price, car_price)
-    if (flag == "Buy_CHE"):
-        buy_symbol(exchange, 'CHE', buyCHE[0][0] + 1, 10)
-        sell_symbol(exchange, 'CAR', sellCAR[0][0] - 1, 10)
-    else:
-        pass
+    book = data_now.read_now_market()
+    buyBAT, sellBAT = book['BAT']
+    buyCAR, sellCAR = book['CAR']
+    buyCHE, sellCHE = book['CHE']
+    buyBDU, sellBDU = book['DBU']
+    buyALI, sellALI = book['ALI']
+    buyTCT, sellTCT = book['TCT']
+    buyBOND, sellBOND = book['BOND']
+    """
+    [[4316, 1], [4314, 1], [4311, 1], [4297, 1], [4294, 3], [4291, 2], [4286, 4]] 买一
+    [[4337, 1], [4338, 1], [4345, 1], [4355, 1]] 卖一
+    """
+
+    if (len(car_price) > 0 and len(che_price) > 0):
+        flag, num = is_exchange_CHE_or_CAR(che_price, car_price)
+        if (flag == "Buy_CHE"):
+            bot.buy_symbol(exchange, 'CHE', buyCHE[0][0] + 1, 10)
+            bot.sell_symbol(exchange, 'CAR', sellCAR[0][0] - 1, 10)
+        else:
+            bot.buy_symbol(exchange, 'CAR', buyCAR[0][0] + 1, 10)
+            bot.sell_symbol(exchange, 'CHE', sellCHE[0][0] - 1, 10)
