8cc5bffd3c388e77948708c1fc92b14f2913d8fa
张定邦 <zdbzuishuai@sjtu.edu.cn>
2019-10-26 11:50:15 +0800

    zdb3

--- readInTrade.py
+++ readInTrade.py
@@ -12,16 +12,17 @@ class Data():
         self.ali = []
         self.tct = []
         self.bat = []
+        self.count = 0 
     
     def get_data(self):
         return self.bond,self.car,self.che,self.bdu,self.ali,self.tct,self.bat
 
     def read_in_trade(self,exchange):
-        count = 0
-        while (count < 200):
+        
+        if (self.count < 200):
             info = read_from_exchange(exchange)
             if not info:
-                break
+                return
             type = info["type"]
             if (type == "close"):
                 print("Server closed.")
@@ -49,5 +50,42 @@ class Data():
                 if (info["symbol"] == "BAT"):
                     self.bat.append(info["price"])
 
-            time.sleep(0.01)
-            count += 1
+
+            self.count += 1
+        else:
+            info = read_from_exchange(exchange)
+            if not info:
+                return
+            type = info["type"]
+            if (type == "close"):
+                print("Server closed.")
+                return
+            if (type == "trade"):
+
+                if (info["symbol"] == "BOND"):
+                    self.bond.append(info["price"])
+                    self.bond.remove(self.bond[0])
+
+                if (info["symbol"] == "CAR"):
+                    self.car.append(info["price"])
+                    self.car.remove(self.car[0])
+
+                if (info["symbol"] == "CHE"):
+                    self.che.append(info["price"])
+                    self.che.remove(self.che[0])
+
+                if (info["symbol"] == "BDU"):
+                    self.bdu.append(info["price"])
+                    self.bdu.remove(self.bdu[0])
+
+                if (info["symbol"] == "ALI"):
+                    self.ali.append(info["price"])
+                    self.ali.remove(self.ali[0])
+
+                if (info["symbol"] == "TCT"):
+                    self.tct.append(info["price"])
+                    self.tct.remove(self.tct[0])
+
+                if (info["symbol"] == "BAT"):
+                    self.bat.append(info["price"])
+                    self.bat.remove(self.bat[0])
